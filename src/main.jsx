de31074f6ec18216e64ab0fc59bf7b7f653d40de
iden0605 <79390850+iden0605@users.noreply.github.com>
2026-03-18 23:03:30 +1100

    Feat: image ux enhancements

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+document.addEventListener('contextmenu', (e) => {
+  if (e.target.tagName === 'IMG') {
+    e.preventDefault()
+  }
+})
+
 // render the main app component
 createRoot(document.getElementById('root')).render(
   <StrictMode>
